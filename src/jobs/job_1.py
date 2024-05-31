8036df3527e7bcb890263b2d109b7c66025ebe3d
Jesse Charbneau <jessecharbneau@gmail.com>
2024-05-31 17:56:47 -0400

    updated to use devices cumulated query and output table

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -4,7 +4,33 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH yesterday AS (
+        SELECT *
+        FROM user_devices_cumulated
+        WHERE date = DATE '2021-02-04'  -- Last updated date
+    ),
+    today AS (
+        SELECT
+            we.user_id,
+            d.browser_type,
+            ARRAY_AGG(DISTINCT DATE_TRUNC('day', we.event_time)) AS dates_active
+        FROM
+            bootcamp.devices d
+            JOIN bootcamp.web_events we ON d.device_id = we.device_id
+        WHERE DATE_TRUNC('day', we.event_time) = DATE '2021-02-05'  -- The next day after the last update
+        GROUP BY
+            we.user_id, d.browser_type
+    )
+    SELECT
+        COALESCE(yesterday.user_id, today.user_id) AS user_id,
+        COALESCE(yesterday.browser_type, today.browser_type) AS browser_type,
+        array_distinct(concat(COALESCE(yesterday.dates_active, ARRAY[]), COALESCE(today.dates_active, ARRAY[]))) AS dates_active,
+        DATE('2021-02-05') AS date  -- Record the date of update to track when the last update occurred.
+    FROM
+        yesterday
+        FULL OUTER JOIN today
+    ON yesterday.user_id = today.user_id
+        AND yesterday.browser_type = today.browser_type
     """
     return query
 
@@ -14,7 +40,7 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "jlcharbneau.user_devices_cumulated"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
