e99cf246c6e9f55f5e8b569ed0bdd512dcae352a
Sai-Mannava <121317962+Sai-Mannava@users.noreply.github.com>
2024-06-01 18:46:17 -0700

    Update job_1.py

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -2,24 +2,28 @@ from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 
-def query_1(output_table_name: str) -> str:
+def query_1(output_table_name: str, year: int) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    SELECT *
+    FROM {output_table_name}
+    WHERE quality_class = 'high' AND is_active = True AND current_year = {year}
     """
     return query
 
-def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_1(output_table_name))
+def job_1(spark_session: SparkSession, output_table_name: str, year: int) -> Optional[DataFrame]:
+    output_df = spark_session.table(output_table_name)
+    output_df.createOrReplaceTempView(output_table_name)
+    return spark_session.sql(query_1(output_table_name, year))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "saisiddu201266140.actors"
+    year: int = 2024
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
-        .appName("job_1")
+        .appName("SelectActiveHighQualityActors")
         .getOrCreate()
     )
-    output_df = job_1(spark_session, output_table_name)
+    output_df = job_1(spark_session, output_table_name, year)
     output_df.write.mode("overwrite").insertInto(output_table_name)
+
