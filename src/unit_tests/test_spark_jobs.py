787af330c1921399c2493e07a174174c22222374
Nancy Castillo <nancycastillo@Nancys-MacBook-Pro.local>
2024-06-02 17:59:05 -0400

    hw4 2

--- src/unit_tests/test_spark_jobs.py
+++ src/unit_tests/test_spark_jobs.py
@@ -5,16 +5,17 @@ from collections import namedtuple
 from pyspark.sql.types import StructType, StructField, StringType, LongType, ArrayType, BooleanType
 
 
+# testing job 1
 
 inputActors = namedtuple("inputActors", "actor quality_class is_active is_active_last_year qualify_class_last_year current_year")
 outputActorsHistory = namedtuple("outputActorsHistory", "actor quality_class is_active start_date end_date current_year")
 
 
 def test_job_1(spark_session):
-    input_table_name: str = "actors"
-    output_table_name: str = "spark_actors_history"
+    input_table_name: str = "actors" # base table
+    output_table_name: str = "spark_actors_history" # write to table
 
-    actual_df = job_2(spark_session, input_table_name, output_table_name)
+    actual_df = job_1(spark_session, input_table_name, output_table_name)
 
     input_table_data = [
         inputActors("Raquel Welch" ,"bad" ,"true" , None , None ,"2014"),
@@ -32,17 +33,18 @@ def test_job_1(spark_session):
 
     output_actors_history_df = spark_session.createDataFrame(output_actors_history)
 
-    expected_df = job_2(spark_session, input_df, output_actors_history_df)
+    expected_df = job_1(spark_session, input_df, output_actors_history_df)
 
     assert_df_equality(actual_df, expected_df)
 
 
+# testing job 2
 input = namedtuple("input", "user_id event_date ct")
 outputCumulative = namedtuple("outputCumulative", "user_id dates_active date")
 
 
 def test_job_2(spark_session):
-    output_table_name: str = "user_devices_comulated"
+    output_table_name: str = "user_devices_comulated" # base table
 
     output_table_name_schema = StructType([
         StructField("user_id", LongType(), nullable=True),
@@ -69,34 +71,3 @@ def test_job_2(spark_session):
 
     assert_df_equality(actual_df, expected_df)
 
-
-# query_1 relates to actors
-
-
-# actors = namedtuple("actors", "actor actor_id quality_class is_active current_year")
-
-# input_data = [
-#         actors(actor="Alexa Demie", actor_id="nm7051533", quality_class="good", is_active="true", current_year="2021"),
-#         actors(actor="Amanda Collin", actor_id="nm4518657", quality_class="average", is_active="true", current_year="2021")
-
-# ]
-
-# def test_query_1(spark_session):
-#     fake_input_data = spark_session.createDataFrame(input_data)
-
-#     # test_output_table_name = "test_actors"
-#     actual_output_data = job_1(spark_session, fake_input_data)
-
-#     expected_schema = StructType([
-#         StructField("actor", StringType(), True),
-#         StructField("actor_id", StringType(), True),
-#         StructField("quality_class", LongType(), True),
-#         StructField("is_active", LongType(), True),
-#         StructField("start_date", StringType(), True),
-#         StructField("end_date", StringType(), True),
-#         StructField("current_year", StringType(), True)
-#     ])
-
-#     assert_df_equality(actual_output_data, )
-#     assert actual_output_data.schema == expected_schema
-# query_2 relates to hosts
