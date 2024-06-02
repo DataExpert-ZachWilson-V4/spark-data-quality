36cfb47e4393d34a7a659e4992112a53de562b47
Brandon Garcia <92387983+bgar8@users.noreply.github.com>
2024-06-02 14:02:07 -0700

    Update job_1.py

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -4,7 +4,43 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH last_year AS (
+    SELECT 
+        * 
+    FROM {output_table_name}
+    WHERE current_year = 1999
+    ), 
+    this_year AS (
+    SELECT
+        actor,
+        actor_id,
+        ARRAY_AGG(ROW(film, votes, rating, film_id, year)) AS films,
+        AVG(rating) AS avg_rating,
+        MAX(year) AS year
+    FROM actor_films
+    WHERE year = 2000
+    GROUP BY actor, actor_id
+    )
+    SELECT 
+        COALESCE(ly.actor, ty.actor) as actor,
+        COALESCE(ly.actor_id, ty.actor_id) as actor_id,
+        CASE
+            WHEN ty.films IS NULL THEN ly.films
+            WHEN ty.films IS NOT NULL and ly.films IS NULL THEN ty.films
+            WHEN ty.films IS NOT NULL and ly.films IS NOT NULL THEN ty.films || ly.films 
+        END as films,
+        CASE
+            WHEN ty.avg_rating > 8 THEN 'star'
+            WHEN ty.avg_rating > 7 and ty.avg_rating <= 8 THEN 'good'
+            WHEN ty.avg_rating > 6 and ty.avg_rating <= 7 THEN 'average'
+            ELSE 'bad'
+        END as quality_class,
+        ty.year IS NOT NULL as is_active,
+        COALESCE(ty.year, ly.current_year + 1) as current_year
+    FROM last_year ly
+    FULL OUTER JOIN this_year ty
+    on ly.actor_id = ty.actor_id
+
     """
     return query
 
@@ -14,7 +50,7 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "actors"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
@@ -23,3 +59,4 @@ def main():
     )
     output_df = job_1(spark_session, output_table_name)
     output_df.write.mode("overwrite").insertInto(output_table_name)
+
