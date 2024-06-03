eef3078227177e2c68c56206b98b2c50c5174dc6
Jesse Charbneau <jessecharbneau@gmail.com>
2024-06-03 12:29:09 -0400

    refactored job1

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -10,6 +10,7 @@ def query_1(events_input_table_name: str, devices_input_table_name: str, output_
         previous_date_str = previous_date.strftime("%Y-%m-%d")
     except ValueError as e:
         print(f"Error parsing date: {e}")
+        return ""
 
     query = f"""
     WITH yesterday AS (
@@ -22,37 +23,35 @@ def query_1(events_input_table_name: str, devices_input_table_name: str, output_
             we.user_id,
             d.browser_type,
             DATE('{current_date}') as todays_date,
-            ARRAY_AGG(DISTINCT DATE_TRUNC('day', we.event_time)) AS dates_active
+            ARRAY_AGG(DATE(we.event_time)) AS event_dates
         FROM
             {devices_input_table_name} d
             JOIN {events_input_table_name} we ON d.device_id = we.device_id
-        WHERE DATE(we.event_time) = DATE({current_date})
+        WHERE DATE(we.event_time) = DATE('{current_date}')
         GROUP BY we.user_id, d.browser_type
     )
     SELECT
-        -- Use COALESCE to handle any null values by choosing data from 'today' if available, otherwise from 'yesterday'.
         COALESCE(yesterday.user_id, today.user_id) AS user_id,
         COALESCE(yesterday.browser_type, today.browser_type) AS browser_type,
-
-        -- Merge and deduplicate date arrays from 'yesterday' and 'today' using the concat function and array_distinct.
-        -- This ensures all unique activity dates are captured in the cumulative table.
-        array_distinct(concat(COALESCE(yesterday.dates_active, ARRAY[]), COALESCE(today.dates_active, ARRAY[]))) AS dates_active,
-
-        COALESCE(today.todays_date, yesterday.date + INTERVAL '1' DAY) AS date  -- Record the date of update to track when the last update occurred.
+        CASE 
+            WHEN yesterday.user_id IS NULL AND today.todays_date IS NOT NULL THEN ARRAY(today.todays_date)
+            WHEN yesterday.dates_active IS NOT NULL AND today.todays_date IS NOT NULL THEN ARRAY(today.todays_date) || yesterday.dates_active
+            WHEN yesterday.dates_active IS NOT NULL AND today.todays_date IS NULL THEN yesterday.dates_active
+            ELSE NULL
+        END AS dates_active,
+        COALESCE(today.todays_date, yesterday.date + INTERVAL 1 DAY) AS date
     FROM
         yesterday
         FULL OUTER JOIN today
         ON yesterday.user_id = today.user_id
         AND yesterday.browser_type = today.browser_type
-    
     """
     return query
 
-def job_1(spark_session: SparkSession,  events_input_table_name: str, devices_input_table_name: str, output_table_name: str, current_date: str) -> Optional[DataFrame]:
-
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_1(events_input_table_name,devices_input_table_name, output_table_name, current_date))
+def job_1(spark_session: SparkSession, events_input_table_name: str, devices_input_table_name: str, output_table_name: str, current_date: str) -> Optional[DataFrame]:
+    output_df = spark_session.table(output_table_name)
+    output_df.createOrReplaceTempView(output_table_name)
+    return spark_session.sql(query_1(events_input_table_name, devices_input_table_name, output_table_name, current_date))
 
 def main():
     output_table_name: str = "jlcharbneau.user_devices_cumulated"
@@ -62,5 +61,8 @@ def main():
         .appName("job_1")
         .getOrCreate()
     )
-    output_df = job_1(spark_session, "web_events", "user_devices", "user_devices_cumulated", "2023-08-20")
+    output_df = job_1(spark_session, "web_events", "user_devices", "user_devices_cumulated", "2023-01-14")
     output_df.write.mode("overwrite").insertInto(output_table_name)
+
+if __name__ == "__main__":
+    main()
