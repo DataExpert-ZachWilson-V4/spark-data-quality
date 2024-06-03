801ad976bc5e72f88356972f9967ea455724be94
shashankkongara <shashankkongara5@gmail.com>
2024-06-03 15:20:12 -0400

    Initial Changes

--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -4,17 +4,79 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    --query 2 from week 2 fact modelling
+    WITH 
+    -- extract data of most recent date
+    yest AS (
+        SELECT  
+            user_id,
+            browser_type,
+            dates_active,
+            date
+        FROM 
+            user_devices_cumulated
+        WHERE 
+            DATE = DATE('2023-01-01')
+    ),
+    
+    -- We join `web_events` with `devices`, because we need to track daily user activity by browser type, we need both user_id and browser_type in the same table
+    joined AS (
+        SELECT 
+            a.user_id,
+            a.event_time,
+            b.browser_type,
+            b.os_type,
+            b.device_type
+        FROM 
+            web_events a
+        INNER JOIN 
+            devices b 
+        ON 
+            a.device_id = b.device_id
+        WHERE 
+            date_trunc('day', event_time) = DATE('2023-01-01') -- Adjust the date to process the next day's events
+    ),
+    
+    -- aggregates event data by user and browser type for today
+    today AS (
+        SELECT 
+            user_id,
+            browser_type,
+            CAST(date_trunc('day', event_time) AS DATE) AS event_date,
+            COUNT(1) AS count_events -- Count of events per user per browser type per day
+        FROM
+            joined 
+        GROUP BY
+            user_id,
+            browser_type,
+            CAST(date_trunc('day', event_time) AS DATE)
+    )
+    -- Append new events to old if there are any, else we create an array of today's events
+    SELECT 
+        COALESCE(y.user_id, t.user_id) AS user_id,
+        COALESCE(y.browser_type, t.browser_type) AS browser_type,
+        CASE
+            WHEN y.dates_active IS NOT NULL THEN array(t.event_date) || y.dates_active
+            ELSE array(t.event_date)
+        END AS dates_active,
+        DATE('2023-01-01') AS date
+    FROM 
+        yest y 
+    FULL OUTER JOIN  
+        today t 
+    ON 
+        y.user_id = t.user_id AND y.browser_type = t.browser_type
     """
     return query
 
+
 def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_2(output_table_name))
+    output_df = spark_session.table(output_table_name)
+    output_df.createOrReplaceTempView(output_table_name)
+    return spark_session.sql(query_2(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "user_devices_cumulated"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
---
+++ src/unit_tests/test_spark_jobs.py
@@ -0,0 +1,145 @@
+import sys
+import os
+from chispa.dataframe_comparer import *
+from collections import namedtuple
+from datetime import date
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    ArrayType,
+    StringType,
+    DoubleType,
+    LongType,
+    BooleanType,
+    TimestampType,
+    DateType
+)
+
+from ..jobs.job_1 import job_1
+from ..jobs.job_2 import job_2
+
+# Define input and output namedtuples for job 1
+InputGame = namedtuple("InputGame", "game_id team_id team_abbreviation team_city player_id player_name nickname start_position comment min fgm fga fg_pct fg3m fg3a fg3_pct ftm fta ft_pct oreb dreb reb ast stl blk to pf pts plus_minus")
+OutputGame = namedtuple("OutputGame", "game_id team_id team_abbreviation team_city player_id player_name nickname start_position comment min fgm fga fg_pct fg3m fg3a fg3_pct ftm fta ft_pct oreb dreb reb ast stl blk to pf pts plus_minus")
+
+def test_job_1(spark_session):
+    schema = StructType([
+        StructField("game_id", LongType(), True),
+        StructField("team_id", LongType(), True),
+        StructField("team_abbreviation", StringType(), True),
+        StructField("team_city", StringType(), True),
+        StructField("player_id", LongType(), True),
+        StructField("player_name", StringType(), True),
+        StructField("nickname", StringType(), True),
+        StructField("start_position", StringType(), True),
+        StructField("comment", StringType(), True),
+        StructField("min", StringType(), True),
+        StructField("fgm", DoubleType(), True),
+        StructField("fga", DoubleType(), True),
+        StructField("fg_pct", DoubleType(), True),
+        StructField("fg3m", LongType(), True),
+        StructField("fg3a", LongType(), True),
+        StructField("fg3_pct", DoubleType(), True),
+        StructField("ftm", LongType(), True),
+        StructField("fta", LongType(), True),
+        StructField("ft_pct", DoubleType(), True),
+        StructField("oreb", LongType(), True),
+        StructField("dreb", LongType(), True),
+        StructField("reb", LongType(), True),
+        StructField("ast", LongType(), True),
+        StructField("stl", LongType(), True),
+        StructField("blk", LongType(), True),
+        StructField("to", LongType(), True),
+        StructField("pf", LongType(), True),
+        StructField("pts", LongType(), True),
+        StructField("plus_minus", DoubleType(), True)
+    ])
+
+    input_data = [
+        (10300001, 1610612762, "UTA", "Utah", 1952, "Raja Bell", None, None, None, "5", 1.0, 5.0, 0.2, 0, 0, 0.0, 0, 0, 0.0, 1, 1, 2, 1, 1, 0, 0, 0, 2, None),
+        (10300001, 1610612762, "UTA", "Utah", 1952, "Raja Bell", None, None, None, "5", 1.0, 5.0, 0.2, 0, 0, 0.0, 0, 0, 0.0, 1, 1, 2, 1, 1, 0, 0, 0, 2, None),
+        (10300001, 1610612762, "UTA", "Utah", 1952, "Raja Bell", None, None, None, "5", 1.0, 5.0, 0.2, 0, 0, 0.0, 0, 0, 0.0, 1, 1, 2, 1, 1, 0, 0, 0, 2, None)
+    ]
+
+    expected_output = [
+        (10300001, 1610612762, "UTA", "Utah", 1952, "Raja Bell", None, None, None, "5", 1.0, 5.0, 0.2, 0, 0, 0.0, 0, 0, 0.0, 1, 1, 2, 1, 1, 0, 0, 0, 2, None)
+    ]
+
+    input_dataframe = spark_session.createDataFrame(input_data, schema)
+    # input_dataframe.createOrReplaceTempView("actors")
+
+    expected_output_dataframe = spark_session.createDataFrame(expected_output, schema)
+
+    actual_df = job_1(spark_session, input_dataframe, "actors")
+    
+    assert_df_equality(actual_df, expected_output_dataframe, ignore_nullable=True)
+
+
+
+
+# Define input and output namedtuples for job 2
+Web_Events = namedtuple("Web_Events", "user_id device_id referrer host url event_time")
+Devices = namedtuple("Devices", "device_id browser_type os_type device_type")
+User_Device_Cumulated = namedtuple("User_Device_Cumulated", "user_id browser_type dates_active date")
+
+def test_job_2(spark_session):
+
+
+
+    #     # Input data based on your provided values
+    # Input_Web_Events = [
+    #     Web_Events(2073894501, 36450401, "https://eczachly.substack.com/", "www.eczachly.com", "/signup", "2023-08-25 06:06:06.721 UTC"),
+    #     Web_Events(542609727, 543560539, "https://www.linkedin.com/", "www.dataengineer.io", "/", "2023-08-25 05:59:56.406 UTC")
+    # ]
+
+    # Input_Devices = [
+    #     Devices(36450401, "Chrome", "Windows", "Other"),
+    #     Devices(543560539, "Mobile Safari UI/WKWebView", "iOS", "iPhone")
+    # ]
+
+    # Input_User_Device_Cumulated_yest = [
+    #     User_Device_Cumulated(2092775577, "Other", [date(2023, 1, 1)], date(2023, 1, 1)),
+    #     User_Device_Cumulated(2073894501, "Chrome", [date(2023, 1, 1)], date(2023, 1, 1))
+    # ]
+
+    # expected_output = [
+    # User_Device_Cumulated(2092775577, "Other", [date(2023, 1, 1)], date(2023, 1, 1)),
+    # User_Device_Cumulated(2073894501, "Chrome", [date(2023, 1, 1)], date(2023, 1, 1)),
+    # User_Device_Cumulated(542609727, "Mobile Safari UI/WKWebView", [date(2023, 8, 25)], date(2023, 1, 1))
+    # ]
+
+    Input_Web_Events = [
+        Web_Events(1078042172, 1088283544, "https://www.eczachly.com/blog/life-of-a-silicon-valley-big-data-engineer-3-critical-soft-skills-for-success", "www.eczachly.com", "/", "2023-01-01 00:03:24.519"),
+        Web_Events(810784131, 532630305, None, "www.eczachly.com", "/contact", "2023-01-01 22:38:47.614")
+    ]
+
+    Input_Devices = [
+        Devices(1088283544, "Other", "Other", "Other"),
+        Devices(532630305, "PetalBot", "Android", "Generic Smartphone")
+    ]
+
+    Input_User_Device_Cumulated_yest = [
+        User_Device_Cumulated(810784131, "PetalBot", [date(2022, 12, 31)], date(2023, 1, 1)),
+        User_Device_Cumulated(1078042172, "Other", [], date(2023, 1, 1))
+    ]
+
+    expected_output = [
+        User_Device_Cumulated(810784131, "PetalBot", [date(2023, 1, 1), date(2022, 12, 31)], date(2023, 1, 1)),
+        User_Device_Cumulated(1078042172, "Other", [date(2023, 1, 1)], date(2023, 1, 1))
+    ]
+
+
+    web_events_df = spark_session.createDataFrame(Input_Web_Events)
+    web_events_df.createOrReplaceTempView("web_events")
+
+    devices_df = spark_session.createDataFrame(Input_Devices)
+    devices_df.createOrReplaceTempView("devices")
+
+    user_devices_cumulated_df = spark_session.createDataFrame(Input_User_Device_Cumulated_yest)
+    user_devices_cumulated_df.createOrReplaceTempView("user_devices_cumulated")
+
+    expected_output_df = spark_session.createDataFrame(expected_output)
+
+    actual_df = job_2(spark_session, "user_devices_cumulated")
+
+    assert_df_equality(actual_df, expected_output_df, ignore_nullable=True)
