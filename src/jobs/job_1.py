30d99efeeea16e4aeeac8dd6dbb687ba79f7f911
Sasiram <bhargavik238@gmail.com>
2024-06-04 00:10:38 +0530

    added homework files

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -1,25 +1,52 @@
 from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.types import *
 
-def query_1(output_table_name: str) -> str:
+
+def query_1(input_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+        WITH CTE AS (
+        -- Assign a unique row number to each combination of game_id, team_id, and player_id
+        SELECT *,
+                ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id ORDER BY dim_game_date) AS row_num
+        FROM {input_table_name}
+        )
+        -- Select all columns except row_num from the CTE
+        SELECT 
+        game_id,
+        team_id,
+        player_id,
+        dim_team_abbreviation,
+        dim_player_name,
+        dim_start_position,
+        dim_did_not_dress,
+        dim_not_with_team,
+        m_seconds_played,
+        m_field_goals_made,
+        m_field_goals_attempted,
+        m_3_pointers_made,
+        m_3_pointers_attempted,
+        m_free_throws_made,
+        m_free_throws_attempted,
+        m_offensive_rebounds,
+        m_defensive_rebounds,
+        m_rebounds,
+        m_assists,
+        m_steals,
+        m_blocks,
+        m_turnovers,
+        m_personal_fouls,
+        m_points,
+        m_plus_minus,
+        dim_game_date,
+        dim_season,
+        dim_team_did_win
+        FROM CTE
+        WHERE row_num = 1  -- Filter out only the rows where the row number is 1. Reason: To filter out unique records without duplicates
     """
     return query
 
-def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_1(output_table_name))
 
-def main():
-    output_table_name: str = "<output table name here>"
-    spark_session: SparkSession = (
-        SparkSession.builder
-        .master("local")
-        .appName("job_1")
-        .getOrCreate()
-    )
-    output_df = job_1(spark_session, output_table_name)
-    output_df.write.mode("overwrite").insertInto(output_table_name)
+def job_1(spark_session: SparkSession,input_table_name: str)-> Optional[DataFrame]:
+  return spark_session.sql(query_1(input_table_name))
