8326018ad25b4ac573adb66406cfa09b570f496e
Amaliah Nation <amaliah.nation@gmail.com>
2024-06-02 20:39:35 -0500

    Commit Jobs

--- tests/test_can_import.py
+++ tests/test_can_import.py
@@ -1,5 +1,7 @@
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 def test_can_import_queries():
-    from src.job_1 import query_1
+    from jobs.job_1 import query_1
     assert query_1 is not None
