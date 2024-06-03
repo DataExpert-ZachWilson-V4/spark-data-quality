fbf67ea14eefd5799147c23aa494ec34a516947d
shettyabhi <62216975+shettyabhi@users.noreply.github.com>
2024-06-02 19:30:47 -0500

    Update job_2.py

--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -2,9 +2,9 @@ from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 
-def query_2(output_table_name: str) -> str:
+def query_2(input_table_name: str) -> str:
     query = f"""
-     with base as 
+    with base as 
   ( 
     SELECT 
         game_id,
@@ -38,7 +38,7 @@ def query_2(output_table_name: str) -> str:
         plus_minus,
         ROW_NUMBER() OVER(PARTITION BY game_id, team_id, player_id) AS dup_row
         from 
-        bootcamp.nba_game_details
+        {input_table_name}
   )
   select 
   game_id,
@@ -76,18 +76,20 @@ def query_2(output_table_name: str) -> str:
     """
     return query
 
-def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_2(output_table_name))
+def job_2(spark_session: SparkSession, input_table_name: str , input_df: DataFrame) -> Optional[DataFrame]:
+  input_df.createOrReplaceTempView(input_table_name)
+  return spark_session.sql(query_2(input_table_name))
+
 
 def main():
-    output_table_name: str = "nba_stats_dedup"
+    input_table_name: str = "nba_game_details"
+    output_table_name: str = "nba_stat_dedup"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
         .appName("job_2")
         .getOrCreate()
     )
-    output_df = job_2(spark_session, output_table_name)
+    input_df = spark_session.table(input_table_name)
+    output_df = job_2(spark_session, input_df, input_table_name)
     output_df.write.mode("overwrite").insertInto(output_table_name)
