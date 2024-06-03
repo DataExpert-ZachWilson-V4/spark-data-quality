645d177f28de4dd021a5f4f91756657f06bdcf98
mandrulliBF <martina.andrulli@burda-forward.it>
2024-06-03 08:52:36 +0200

    Add job 1

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -4,7 +4,43 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH last_year_cte AS (
+        SELECT *
+        FROM 
+            {output_table_name}
+        WHERE 
+            current_year=1913 
+    ),
+    this_year_agg_cte AS (
+        SELECT 
+            ty.actor, 
+            ty.actor_id,
+            ARRAY_AGG(ROW(film, votes, rating, film_id, year)) as films, 
+            CASE 
+                WHEN AVG(rating) > 8 THEN 'star' 
+                WHEN AVG(rating) > 7 THEN 'good' 
+                WHEN AVG(rating) > 6 THEN 'average' 
+            ELSE 'bad' 
+            END as quality_class,
+            ty.year as year
+        FROM actor_films AS ty
+        WHERE year = 1914
+        GROUP BY ty.actor, ty.actor_id, ty.year
+    )
+    SELECT 
+        COALESCE(ly.actor, tya.actor) AS actor, 
+        COALESCE(ly.actor_id, tya.actor_id) AS actor_id,
+        CASE
+            WHEN ly.films IS NULL AND tya.films IS NOT NULL THEN tya.films 
+            WHEN ly.films IS NOT NULL AND tya.films IS NOT NULL THEN tya.films || ly.films 
+            WHEN tya.films IS NULL THEN ly.films 
+        END AS films,
+        COALESCE (tya.quality_class, ly.quality_class) AS quality_class, 
+        tya.year IS NOT NULL AS is_active, 
+        COALESCE(tya.year, ly.current_year + 1) AS current_year 
+    FROM last_year_cte AS ly
+    FULL OUTER JOIN this_year_agg_cte AS tya 
+        ON ly.actor_id = tya.actor_id 
     """
     return query
 
@@ -14,7 +50,7 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "actors"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
