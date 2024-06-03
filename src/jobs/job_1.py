f1eef17794cb031ecaac511b47459f1f010240a0
Ovo <ojameruayeovo@gmail.com>
2024-06-02 19:10:47 -0600

    homework4 initial commit

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -4,7 +4,48 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH 
+        previous_year AS (
+            SELECT * 
+            FROM {output_table_name}
+            WHERE current_year = 2013
+        ),
+
+        current_year AS (
+            SELECT actor, 
+                actor_id, 
+                year, 
+                COLLECT_LIST(STRUCT(year, film, votes, rating, film_id)) AS films,
+                AVG(rating) AS avg_rating
+            FROM  actor_films
+            WHERE year = 2014
+            GROUP BY actor, actor_id, year
+        )
+
+
+    SELECT COALESCE(py.actor, cy.actor) AS actor,
+        COALESCE(py.actor_id, cy.actor_id) AS actor_id,
+        CASE
+            WHEN cy.year IS NULL THEN py.films
+            WHEN cy.year IS NOT NULL 
+                AND py.films IS NULL 
+                THEN cy.films 
+            WHEN cy.year IS NOT NULL 
+                AND py.films IS NOT NULL 
+                THEN cy.films || py.films 
+        END AS films,
+        CASE 
+            WHEN cy.avg_rating IS NULL THEN py.quality_class  
+            WHEN cy.avg_rating > 8 THEN 'star'
+            WHEN cy.avg_rating > 7 AND cy.avg_rating <= 8 THEN 'good'
+            WHEN cy.avg_rating > 6 AND cy.avg_rating <= 7 THEN 'average'
+            WHEN cy.avg_rating <= 6 THEN 'bad'
+        END AS quality_class,
+        cy.year IS NOT NULL AS is_active,
+        COALESCE(cy.year, py.current_year + 1) as current_year
+    FROM previous_year py
+    FULL OUTER JOIN current_year cy 
+        ON py.actor_id = cy.actor_id
     """
     return query
 
@@ -14,7 +55,7 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "actors"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -4,7 +4,33 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH
+    previous_date_records AS (
+        SELECT *
+        FROM {output_table_name}
+        WHERE date = DATE('2023-01-06')  
+    ),
+
+    current_date_records AS (
+        SELECT e.user_id,
+        d.browser_type,
+        CAST(date_trunc('day', e.event_time) AS DATE) AS event_date
+        FROM web_events e
+        LEFT JOIN devices d ON d.device_id = e.device_id
+        WHERE date_trunc('day', e.event_time) = DATE('2023-01-07')
+        GROUP BY e.user_id, d.browser_type, CAST(date_trunc('day', e.event_time) AS DATE)
+    )
+
+    SELECT 
+    COALESCE(p.user_id, c.user_id) AS user_id,
+    COALESCE(p.browser_type, c.browser_type) AS browser_type,
+    CASE 
+        WHEN p.dates_active IS NOT NULL THEN ARRAY(c.event_date) || p.dates_active 
+        ELSE ARRAY(c.event_date)
+    END AS dates_active,
+    DATE('2023-01-07') AS date
+    FROM previous_date_records p
+    FULL OUTER JOIN current_date_records c ON c.user_id = p.user_id AND c.browser_type = p.browser_type
     """
     return query
 
@@ -14,7 +40,7 @@ def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_2(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "user_devices_cummulated"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
