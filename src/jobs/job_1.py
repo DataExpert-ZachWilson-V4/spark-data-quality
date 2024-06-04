fe2eef3c9f9fc2872075a006378678cc81e6cbe3
meetapandit <meeta.pandit890@gmail.com>
2024-06-03 17:06:27 -0700

    upload homework_4

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -1,25 +1,105 @@
 from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    DoubleType,
+    LongType,
+)
+import shutil
+import os
 
-def query_1(output_table_name: str) -> str:
+schema = StructType(
+    [
+        StructField("game_id", LongType(), True),
+        StructField("team_id", LongType(), True),
+        StructField("team_abbreviation", StringType(), True),
+        StructField("team_city", StringType(), True),
+        StructField("player_id", LongType(), True),
+        StructField("player_name", StringType(), True),
+        StructField("nickname", StringType(), True),
+        StructField("start_position", StringType(), True)
+    ]
+)
+
+
+def create_output_table(spark_session, table_name):
+    
+    spark_session.sql(f"DROP TABLE IF EXISTS {table_name}")
+    # Get the warehouse location
+    warehouse_location = spark_session.conf.get("spark.sql.warehouse.dir", "spark-warehouse")
+
+    # Construct the table folder path
+    table_folder_path = os.path.join(warehouse_location, table_name)
+    print("table_folder_path", table_folder_path)
+    
+    # Check if the folder exists and delete it
+    if os.path.exists(table_folder_path):
+        shutil.rmtree(table_folder_path)
+        print(f"Table folder '{table_folder_path}' has been deleted.")
+    else:
+        print(f"Table folder '{table_folder_path}' does not exist.")
+        tables = spark_session.catalog.listTables()
+        print(f"Tables in the default database after dropping {table_name}:")
+
+    tables = spark_session.catalog.listTables()   
+    for table in tables:
+        print(table.name)
+
+    if table_name not in tables:
+        empty_df = spark_session.createDataFrame([], schema)
+        empty_df.write.mode("overwrite").saveAsTable(table_name)   
+    return spark_session.table(table_name)
+
+
+def query_1(input_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+        WITH
+            row_nums AS (
+                SELECT
+                    *,
+                    ROW_NUMBER() OVER (
+                        PARTITION BY
+                            game_id,
+                            team_id,
+                            player_id
+                    ORDER BY game_id) AS row_number
+                FROM
+                    {input_table_name}
+            )
+        SELECT
+            game_id,
+            team_id,
+            team_abbreviation,
+            team_city,
+            player_id,
+            player_name,
+            nickname,
+            start_position
+        FROM
+            row_nums
+        WHERE
+            row_number = 1
     """
     return query
 
-def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_1(output_table_name))
+
+def job_1(
+    spark_session: SparkSession, input_table_name: str, output_table_name: str) -> Optional[DataFrame]:
+    output_df = create_output_table(spark_session, output_table_name)
+    output_df.createOrReplaceTempView(output_table_name)
+    return spark_session.sql(query_1(input_table_name))
+
 
 def main():
-    output_table_name: str = "<output table name here>"
+    input_table_name: str = "nba_game_details"
+    output_table_name: str = "nba_game_dedupe"
     spark_session: SparkSession = (
-        SparkSession.builder
-        .master("local")
-        .appName("job_1")
-        .getOrCreate()
+        SparkSession.builder.master("local").appName("job_1").getOrCreate()
     )
-    output_df = job_1(spark_session, output_table_name)
-    output_df.write.mode("overwrite").insertInto(output_table_name)
+    output_df = job_1(spark_session, input_table_name, output_table_name)
+    output_df.write.option(
+        "path", spark_session.conf.get("spark.sql.warehouse.dir", "spark-warehouse")
+    ).mode("overwrite").insertInto(output_table_name)
--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -2,24 +2,72 @@ from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 
-def query_2(output_table_name: str) -> str:
+def query_2(output_table_name: str, input_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH
+    last_year AS (
+        SELECT *
+        FROM {output_table_name}
+        WHERE
+        current_year = 2018
+    )
+    -- CTE to read data from new incoming data which
+    -- is not in actors table
+    , this_year AS (
+        SELECT DISTINCT *
+        FROM {input_table_name}
+        WHERE year = 2019
+    )
+    , average_rating_ty AS (
+  SELECT actor
+       , actor_id
+       , COLLECT_LIST(ARRAY(year, film, votes, rating, film_id)) AS films
+       , AVG(rating) AS avg_rating
+       , MAX(year) AS current_year
+  FROM this_year
+  GROUP BY actor
+       , actor_id
+  )
+    SELECT COALESCE(ly.actor, ty.actor) AS actor
+        , COALESCE(ly.actor_id, ty.actor_id) AS actor_id
+        -- actor not present in new dataset
+        , CASE WHEN ty.current_year IS NULL THEN ly.films
+        -- new actor record
+                WHEN ty.current_year IS NOT NULL AND ly.films IS NULL THEN ty.films
+                -- present in both last year and this year       
+                -- so append array
+                WHEN ty.current_year IS NOT NULL AND ly.films IS NOT NULL 
+                THEN ty.films || ly.films
+
+            END AS films
+        , CASE WHEN ty.avg_rating IS NULL THEN ly.quality_class 
+        ELSE (
+            CASE WHEN ty.avg_rating > 8 THEN 'star'
+                WHEN ty.avg_rating > 7 AND ty.avg_rating <= 8  THEN 'good'
+                WHEN ty.avg_rating > 6 AND ty.avg_rating <= 7  THEN 'average'
+                WHEN ty.avg_rating <= 6 THEN 'bad'
+            END)
+            END AS quality_class
+        , ty.current_year IS NOT NULL AS is_active
+        , COALESCE(ty.current_year, ly.current_year + 1) AS current_year
+    FROM last_year ly
+    FULL OUTER JOIN average_rating_ty ty ON ly.actor_id = ty.actor_id
     """
     return query
 
-def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
+def job_2(spark_session: SparkSession, output_table_name: str, input_table_name: str) -> Optional[DataFrame]:
   output_df = spark_session.table(output_table_name)
   output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_2(output_table_name))
+  return spark_session.sql(query_2(output_table_name, input_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "actors"
+    input_table_name = "actor_films"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
         .appName("job_2")
         .getOrCreate()
     )
-    output_df = job_2(spark_session, output_table_name)
+    output_df = job_2(spark_session, output_table_name, input_table_name)
     output_df.write.mode("overwrite").insertInto(output_table_name)
