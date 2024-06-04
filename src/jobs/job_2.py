45fd9a7dd39777b115435e18c6aa45d608adbd7f
supaman-go <154898872+supaman-go@users.noreply.github.com>
2024-06-03 21:28:48 -0700

    Added Job 1 & 2 with spark test

--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -4,7 +4,17 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH row_records AS (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER(PARTITION BY game_id, team_id, player_id) as row_num
+    FROM bootcamp.nba_game_details
+    )
+    SELECT 
+        *
+    FROM row_records
+    -- Only the first row is necessary
+    WHERE row_num = 1     
     """
     return query
 
@@ -14,12 +24,15 @@ def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_2(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "nba_game_dedupe"
+    input_table_name: str = "nba_game_details"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
         .appName("job_2")
         .getOrCreate()
     )
-    output_df = job_2(spark_session, output_table_name)
-    output_df.write.mode("overwrite").insertInto(output_table_name)
+    output_df = job_2(spark_session,input_table_name, output_table_name)
+    output_df.write.option(
+        "path", spark_session.conf.get("spark.sql.warehouse.dir", "spark-warehouse")
+    ).mode("overwrite").insertInto(output_table_name)
