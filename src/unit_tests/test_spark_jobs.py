596c2e533adfa4202204a030c959bd0c2ecd8fb5
sumanacheera <sumanacheera8@gmail.com>
2024-06-04 01:07:49 -0400

    Update test_spark_jobs.py

--- src/unit_tests/test_spark_jobs.py
+++ src/unit_tests/test_spark_jobs.py
@@ -53,68 +53,40 @@ def test_job_1(spark_session):
         spark_session("job_1"), "nba_game_details", nba_game_details_df
     )
     assert_df_equality(query_output, nba_game_details_deduped_df)
+    
+
+PlayerSeason = namedtuple("PlayerSeason", "player_name is_active current_season")
+PlayerScd = namedtuple("PlayerScd", "player_name is_active start_season end_season")
+
 
 def test_job_2(spark_session):
-    # Create DataFrames for user_devices_cumulated and web_events + devices for testing
-    web_events = namedtuple("web_events", "user_id device_id referrer host url event_time")
-    devices = namedtuple("devices", "device_id browser_type os_type device_type")
-    user_devices_cumulated = namedtuple("user_devices_cumulated", "user_id browser_type dates_active date")
+    input_table_name: str = "nba_players"
+    source_data = [
+        PlayerSeason("Michael Jordan", True, 2001),
+        PlayerSeason("Michael Jordan", True, 2002),
+        PlayerSeason("Michael Jordan", True, 2003),
+        PlayerSeason("Michael Jordan", False, 2004),
+        PlayerSeason("Michael Jordan", False, 2005),
+        PlayerSeason("Scottie Pippen", True, 2001),
+        PlayerSeason("Scottie Pippen", False, 2002),
+        PlayerSeason("Scottie Pippen", False, 2003),
+        PlayerSeason("Scottie Pippen", True, 2004),
+        PlayerSeason("Scottie Pippen", True, 2005),
+        PlayerSeason("LeBron James", True, 2003),
+        PlayerSeason("LeBron James", True, 2004),
+        PlayerSeason("LeBron James", True, 2005)
+    ]
+    source_df = spark_session.createDataFrame(source_data)
 
-        # Input data for testing
-input_events_data = [
-            web_events(
-                user_id = 495022226,
-                device_id = -2012543895, 
-                referrer = NULL, 
-                host = 'www.zachwilson.tech', 
-                url = '/', 
-                event_time = datetime(2023, 1, 3, 0, 31, tzinfo=pytz.UTC)
-            )
-        ]
-
-input_devices_data = [
-            devices(
-                device_id = -2012543895, 
-                browser_type = 'Googlebot', 
-                os_type = 'Other', 
-                device_type = 'Spider'
-            )
-        ]
-
-# Existing data for testing
-        
-existing_user_devices_cumulated_data = [
-            user_devices_cumulated(
-                user_id = 495022226,
-                browser_type = 'Googlebot',
-                dates_active = [datetime.strptime("2023-01-02", "%Y-%m-%d").date()],
-                date = datetime.strptime("2023-01-02", "%Y-%m-%d").date()
-            )
-        ]
-
-# Expected data for testing
-expected_data = [
-            user_devices_cumulated(
-                user_id = 495022226,
-                browser_type = 'Googlebot',
-                dates_active = [datetime.strptime("2023-01-03", "%Y-%m-%d").date(), datetime.strptime("2023-01-02", "%Y-%m-%d").date()],
-                date = datetime.strptime("2023-01-03", "%Y-%m-%d").date()
-            )
-        ]
-
-        
-# DataFrame of the input and existing data and views the test will reference
-input_events_df = spark_session.createDataFrame(input_events_data)
-input_events_df.createOrReplaceTempView("web_events")
-
-input_devices_df = spark_session.createDataFrame(input_devices_data)
-input_devices_df.createOrReplaceTempView("devices")
-
-existing_df = spark_session.createDataFrame(existing_user_devices_cumulated_data)
-existing_df.createOrReplaceTempView("user_devices_cumulated")
-
-# Run the Update_User_Devices_Cumulated function and compare the output with expected
-actual_df = Update_User_Devices_Cumulated(spark_session, "user_devices_cumulated")
-expected_df = spark_session.createDataFrame(expected_data)
-assert_df_equality(actual_df, expected_df, ignore_nullable=True)        
-        
+    from jobs.job_2 import job_2
+    actual_df = job_2(spark_session, source_df, input_table_name)
+    expected_data = [
+        PlayerScd("Michael Jordan", True, 2001, 2003),
+        PlayerScd("Michael Jordan", False, 2004, 2005),
+        PlayerScd("Scottie Pippen", True, 2001, 2001),
+        PlayerScd("Scottie Pippen", False, 2002, 2003),
+        PlayerScd("Scottie Pippen", True, 2004, 2005),
+        PlayerScd("LeBron James", True, 2003, 2005)
+    ]
+    expected_df = spark_session.createDataFrame(expected_data)
+    assert_df_equality(actual_df.sort("player_name", "start_season"), expected_df.sort("player_name", "start_season"))
