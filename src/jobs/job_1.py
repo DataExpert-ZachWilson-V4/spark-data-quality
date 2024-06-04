1d373abc6fc0fcaa556b3ef1254b832c1d1bf684
AliaHa3 <eng.alia.hamwi@gmail.com>
2024-06-04 03:03:39 +0300

    formatting and lintting code

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -2,11 +2,11 @@ from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 
+
 # The query from week2-Query1 to remove duplication in data
 def query_1(input_table_name: str) -> str:
     query = f"""
     select
-
         game_id
         , team_id
         , player_id
@@ -31,18 +31,19 @@ def query_1(input_table_name: str) -> str:
     """
     return query
 
-def job_1(spark_session: SparkSession, input_table_name: str, input_dataframe: DataFrame) -> Optional[DataFrame]:
-  input_dataframe.createOrReplaceTempView(input_table_name)
-  return spark_session.sql(query_1(input_table_name))
+
+def job_1(
+    spark_session: SparkSession, input_table_name: str, input_dataframe: DataFrame
+) -> Optional[DataFrame]:
+    input_dataframe.createOrReplaceTempView(input_table_name)
+    return spark_session.sql(query_1(input_table_name))
+
 
 def main():
     input_table_name: str = "nba_game_details"
     output_table_name: str = "nba_game_details_deduped"
     spark_session: SparkSession = (
-        SparkSession.builder
-        .master("local")
-        .appName("job_1")
-        .getOrCreate()
+        SparkSession.builder.master("local").appName("job_1").getOrCreate()
     )
     input_dataframe = spark_session.table(input_table_name)
     output_df = job_1(spark_session, input_table_name, input_dataframe)
--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -1,10 +1,14 @@
+import datetime
 from typing import Optional
+
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
-import datetime
+
 
 # The query from week2-Query6 to incrementally populate the hosts_cumulated table from the web_events table
-def query_2(cumulated_table_name: str,daily_table_name: str,yesterday: str,today: str) -> str:
+def query_2(
+    cumulated_table_name: str, daily_table_name: str, yesterday: str, today: str
+) -> str:
     query = f"""
         WITH yesterday_cte AS (
             SELECT
@@ -40,15 +44,16 @@ def query_2(cumulated_table_name: str,daily_table_name: str,yesterday: str,today
     """
     return query
 
-def job_2 (spark_session: SparkSession,
-          cumulated_dataframe: DataFrame ,
-          cumulated_table_name: str ,
-          daily_dataframe: DataFrame ,
-          daily_table_name: str ,
-          today: str
-        ) -> Optional[DataFrame]:
 
-    date_format = '%Y-%m-%d'
+def job_2(
+    spark_session: SparkSession,
+    cumulated_dataframe: DataFrame,
+    cumulated_table_name: str,
+    daily_dataframe: DataFrame,
+    daily_table_name: str,
+    today: str,
+) -> Optional[DataFrame]:
+    date_format = "%Y-%m-%d"
     today = datetime.datetime.strptime(str(today), date_format).date()
     yesterday = today - datetime.timedelta(days=1)
 
@@ -58,24 +63,30 @@ def job_2 (spark_session: SparkSession,
     cumulated_dataframe.createOrReplaceTempView(cumulated_table_name)
     daily_dataframe.createOrReplaceTempView(daily_table_name)
 
-    return spark_session.sql(query_2(cumulated_table_name,daily_table_name,yesterday,today))
+    return spark_session.sql(
+        query_2(cumulated_table_name, daily_table_name, yesterday, today)
+    )
+
 
 def main():
     cumulated_table_name: str = "hosts_cumulated"
     daily_table_name: str = "web_events"
 
     spark_session: SparkSession = (
-        SparkSession.builder
-        .master("local")
-        .appName("job_2")
-        .getOrCreate()
+        SparkSession.builder.master("local").appName("job_2").getOrCreate()
     )
-    spark_session.conf.set('spark.sql.session.timeZone', 'UTC')
+    spark_session.conf.set("spark.sql.session.timeZone", "UTC")
     cumulated_df = spark_session.table(cumulated_table_name)
     daily_df = spark_session.table(daily_table_name)
     today = str(datetime.date.today())
 
-    output_df = job_2(spark_session, cumulated_df,cumulated_table_name,
-                      daily_df,daily_table_name,today)
-    
+    output_df = job_2(
+        spark_session,
+        cumulated_df,
+        cumulated_table_name,
+        daily_df,
+        daily_table_name,
+        today,
+    )
+
     output_df.write.mode("append").insertInto(cumulated_table_name)
