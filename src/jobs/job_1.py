032546c2e57e1bf563f71be3d35846bafc287b68
TejalK14 <tejal.kanase1404@gmail.com>
2024-06-04 00:20:47 -0400

    k 	ok :wq

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -2,24 +2,51 @@ from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 
-def query_1(output_table_name: str) -> str:
-    query = f"""
-    <YOUR QUERY HERE>
+#
+def query_1(input_table_name: str) -> str:
+    return f"""
+            WITH
+        actorfilms AS ( -- get aggregated data for avg rating for each actor per year
+        SELECT
+        actor
+        ,actor_id 
+        ,AVG(rating) as avg_rating
+        ,year as current_year
+        FROM 
+        {input_table_name}
+        WHERE
+        year = 1917
+        group by actor,actor_id,year
+    )
+    SELECT 
+    actor,
+    actor_id,
+    CASE WHEN avg_rating IS NOT NULL THEN
+        CASE WHEN avg_rating > 8 THEN 'star'
+            WHEN avg_rating > 7 THEN 'good'
+            WHEN avg_rating > 6 THEN 'average'
+            WHEN avg_rating <= 6 THEN 'bad' END
+        ELSE NULL END as quality_class,
+    current_year
+    FROM
+    actorfilms
     """
-    return query
+    return  query
 
-def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_1(output_table_name))
+
+def job_1(spark_session: SparkSession, input_df: DataFrame, input_table_name: str) -> Optional[DataFrame]:
+    input_df.createOrReplaceTempView(input_table_name)
+    return spark_session.sql(query_1(input_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    input_table_name: str = "actor_films"
+    output_table_name: str = "actor_films_agg"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
         .appName("job_1")
         .getOrCreate()
     )
-    output_df = job_1(spark_session, output_table_name)
+    input_df = spark_session.table(input_table_name)
+    output_df = job_1(spark_session, input_df, input_table_name)
     output_df.write.mode("overwrite").insertInto(output_table_name)
---
+++ src/unit_tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from pyspark.sql import SparkSession
+
+@pytest.fixture(scope='session')
+def spark_session():
+    return SparkSession.builder \
+      .master("local") \
+      .appName("chispa") \
+      .getOrCreate()
+
+# def spark_session_factory(app_name: str) -> SparkSession:
+#   return (
+#       SparkSession.builder
+#       .master("local")
+#       .appName("chispa")
+#       .getOrCreate()
+#   )
+
+# @pytest.fixture(scope='session')
+# def spark_session():
+#     return spark_session_factory
