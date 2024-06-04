cb9725bf4b54978e922a33ed07dd314b03bfa129
Shabab <22752622+shababmoali@users.noreply.github.com>
2024-06-04 00:08:15 -0700

    add test import check for job 2

--- tests/test_can_import.py
+++ tests/test_can_import.py
@@ -1,6 +1,7 @@
 from src.jobs.job_1 import query_1
-# from src.jobs.job_1 import job_1
+from src.jobs.job_2 import query_2
 
 
 def test_can_import_queries():
     assert query_1 is not None
+    assert query_2 is not None
