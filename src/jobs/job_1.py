c815427275aa816c1988f6bd21b0e72dc414c3b4
bhautikg <52224896+bhautikg@users.noreply.github.com>
2024-06-03 18:12:10 -0700

    submission

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -3,8 +3,57 @@ from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
+    # This query populates actors table using actor_films table
     query = f"""
-    <YOUR QUERY HERE>
+        WITH last_year AS (
+            SELECT * FROM {output_table_name}
+            WHERE current_year = 1914
+            UNION ALL
+            SELECT NULL, NULL, NULL, NULL, NULL, NULL
+            WHERE NOT EXISTS (SELECT * FROM {output_table_name} WHERE current_year = 1914)
+        ),
+
+        this_year AS (
+            SELECT
+                a.actor,
+                a.actor_id,
+                year AS current_year,
+                true AS is_active,
+                collect_list(named_struct('year', a.year, 'film', a.film, 'votes', a.votes, 'rating', a.rating, 'film_id', a.film_id)) AS films,
+                AVG(rating) AS avg_rating
+            FROM
+                actor_films AS a
+            WHERE a.year = 1915
+            GROUP BY
+                
+                a.actor_id, a.actor, year
+        )
+
+        SELECT
+            COALESCE(ly.actor, ty.actor) AS actor,
+            COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
+            CASE
+                WHEN ty.films IS null THEN ly.films
+                WHEN
+                    ty.films IS NOT null
+                    AND ly.films IS null THEN ty.films
+                WHEN
+                    ty.films IS NOT null
+                    AND ly.films IS NOT null THEN ty.films || ly.films
+            END AS films,
+            CASE
+                WHEN ty.films IS null THEN ly.quality_class
+                WHEN ty.avg_rating > 8 THEN 'star'
+                WHEN ty.avg_rating > 7 AND ty.avg_rating <= 8 THEN 'good'
+                WHEN ty.avg_rating > 6 AND ty.avg_rating <= 7 THEN 'average'
+                WHEN ty.avg_rating <= 6 THEN 'bad'
+            END AS quality_class,
+            ty.current_year IS NOT null AS is_active,
+            COALESCE(ty.current_year, ly.current_year + 1) AS current_year
+        FROM last_year AS ly
+
+        FULL OUTER JOIN this_year AS ty
+            ON ly.actor_id = ty.actor_id
     """
     return query
 
@@ -14,7 +63,7 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "actors"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
---
+++ src/unit_tests/test_spark_jobs.py
@@ -0,0 +1,219 @@
+from datetime import datetime
+from chispa.dataframe_comparer import assert_df_equality
+import pytz
+from pyspark.sql import Row
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, BooleanType, ArrayType, IntegerType, DateType, TimestampType
+from ..jobs.job_1 import job_1
+from ..jobs.job_2 import job_2
+
+def test_job1(spark_session):
+    output_table_name: str = "actors"
+   # Create DataFrame for actors table schema, so that data types are correct
+    actors_schema = StructType([
+        StructField("actor", StringType(), True),
+        StructField("actor_id", StringType(), True),
+        StructField("films", ArrayType(
+            StructType([
+                StructField("year", IntegerType(), True),
+                StructField("film", StringType(), True),
+                StructField("votes", IntegerType(), True),
+                StructField("rating", DoubleType(), True),
+                StructField("film_id", StringType(), True)
+            ])
+        ), True),
+        StructField("quality_class", StringType(), True),
+        StructField("is_active", BooleanType(), False),
+        StructField("current_year", IntegerType(), True)
+    ])
+    #create yesterdays data for actors table
+    yesterday_actors_data = [
+        Row(
+            actor = 'Actor A',
+            actor_id = 'actorid1',
+            films = [
+                Row(
+                    year = 1914,
+                    film = 'actorAfilm1',
+                    votes = 1000,
+                    rating = 6.1,
+                    film_id = 'filmid1'
+                ),
+                Row(
+                    year = 1914,
+                    film = 'actorAfilm2',
+                    votes = 200,
+                    rating = 5.8,
+                    film_id = 'filmid2'
+                )
+            ],
+            quality_class = 'bad',
+            is_active = True,
+            current_year = 1914
+        )
+    ]
+    yesterday_df = spark_session.createDataFrame(yesterday_actors_data, actors_schema)
+    yesterday_df.createOrReplaceTempView(output_table_name)
+       
+    # Define schema for actor_films DataFrame
+    actor_films_schema = StructType([
+        StructField("actor", StringType(), True),
+        StructField("actor_id", StringType(), True),
+        StructField("film", StringType(), True),
+        StructField("year", IntegerType(), True),
+        StructField("votes", IntegerType(), True),
+        StructField("rating", DoubleType(), True),
+        StructField("film_id", StringType(), True),
+    ])
+
+    # Input data for testing
+    actor_films_data = [
+        Row(
+            actor='Actor A',
+            actor_id = 'actorid1',
+            film = 'actorAfilm3',
+            year = 1915,
+            votes = 300,
+            rating = 6.3,
+            film_id = 'filmid3'
+        ),
+        Row(
+            actor='Actor B',
+            actor_id = 'actorid2',
+            film = 'actorBfilm1',
+            year = 1915,
+            votes = 500,
+            rating = 8.0,
+            film_id = 'filmid4'
+        )
+    ]
+    actor_films_df = spark_session.createDataFrame(actor_films_data, actor_films_schema)
+    actor_films_df.createOrReplaceTempView('actor_films')
+
+    # Create DataFrame for expected data
+    # Expected data for testing
+    # expect actor A to have a new film in their films array in the front
+    # expect actor B to appear for the first time with one film
+    expected_data = [
+        Row(
+            actor = 'Actor A',
+            actor_id = 'actorid1',
+            films = [
+                Row(
+                    year = 1915,
+                    film = 'actorAfilm3',
+                    votes = 300,
+                    rating = 6.3,
+                    film_id = 'filmid3'
+                ),
+                Row(
+                    year = 1914,
+                    film = 'actorAfilm1',
+                    votes = 1000,
+                    rating = 6.1,
+                    film_id = 'filmid1'
+                ),
+                Row(
+                    year = 1914,
+                    film = 'actorAfilm2',
+                    votes = 200,
+                    rating = 5.8,
+                    film_id = 'filmid2'
+                )
+            ],
+            quality_class = 'average',
+            is_active = True,
+            current_year = 1915
+        ),
+        Row(
+            actor = 'Actor B',
+            actor_id = 'actorid2',
+            films = [
+                Row(
+                    year = 1915,
+                    film = 'actorBfilm1',
+                    votes = 500,
+                    rating = 8.0,
+                    film_id = 'filmid4'
+                )
+            ],
+            quality_class = 'good',
+            is_active = True,
+            current_year = 1915
+        )
+    ]
+    expected_df = spark_session.createDataFrame(expected_data, actors_schema)
+
+    actual_df = job_1(spark_session, output_table_name)
+
+    assert_df_equality(actual_df, expected_df)
+
+def test_job2(spark_session):
+    output_table_name: str = "hosts_cumulated"
+    # create schema for hosts_cumulated table so that datatypes are correct
+    hosts_cumulated_schema = StructType([
+        StructField("host", StringType(), True),
+        StructField("host_activity_datelist", ArrayType(DateType()), True),
+        StructField("date", DateType(), True)
+    ])
+
+    # insert yesterdays hosts_cumulated data
+    yesterday_hosts_cumulated_data = [
+        Row(
+            host = 'host1.com',
+            host_activity_datelist = [datetime.strptime('2022-12-31', "%Y-%m-%d").date()],
+            date = datetime.strptime('2022-12-31', "%Y-%m-%d").date()
+        )
+    ]
+    yesterday_df = spark_session.createDataFrame(yesterday_hosts_cumulated_data,hosts_cumulated_schema)
+    yesterday_df.createOrReplaceTempView(output_table_name)
+    # Get the UTC timezone
+    utc_timezone = pytz.utc
+    # create input web_events data schema for proper data types
+    web_events_schema = StructType([
+        StructField("user_id", IntegerType(), True),
+        StructField("device_id", IntegerType(), False),
+        StructField("referrer", StringType(), True),
+        StructField("host", StringType(), False),
+        StructField("url", StringType(), False),
+        StructField("event_time", TimestampType(), False)
+    ])
+    # create web events data for today
+    web_events_data = [
+        Row(
+            user_id = 123,
+            device_id = 567,
+            referrer = None,
+            host = 'host1.com',
+            url = '/',
+            event_time = datetime(2023, 1, 1, 10, 30, 0, 0, tzinfo=utc_timezone)
+        ),
+        Row(
+            user_id = 123,
+            device_id = 567,
+            referrer = None,
+            host = 'abcd.com',
+            url = '/',
+            event_time = datetime(2023, 1, 1, 10, 30, 0, 0, tzinfo=utc_timezone)
+        )
+    ]
+    web_events_df = spark_session.createDataFrame(web_events_data,web_events_schema)
+    web_events_df.createOrReplaceTempView('web_events')
+
+    # create expected output data
+    expected_data = [
+        Row(
+            host = 'abcd.com',
+            host_activity_datelist = [datetime.strptime('2023-01-01', "%Y-%m-%d").date()],
+            date = datetime.strptime('2023-01-01', "%Y-%m-%d").date()
+        ),
+        Row(
+            host = 'host1.com',
+            host_activity_datelist = [datetime.strptime('2023-01-01', "%Y-%m-%d").date(), datetime.strptime('2022-12-31', "%Y-%m-%d").date()],
+            date = datetime.strptime('2023-01-01', "%Y-%m-%d").date()
+        )
+    ]
+    expected_df = spark_session.createDataFrame(expected_data,hosts_cumulated_schema)
+
+    actual_df = job_2(spark_session, output_table_name)
+
+    assert_df_equality(actual_df, expected_df)
