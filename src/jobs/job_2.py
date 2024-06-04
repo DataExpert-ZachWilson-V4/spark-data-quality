7ec182a20bba7800ffc4d9b6feac026c4577a7e7
sumanacheera <sumanacheera8@gmail.com>
2024-06-03 22:55:16 -0400

    Update job_2.py

--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -4,7 +4,30 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH yesterday AS (
+        SELECT *
+        FROM {output_table_name}
+        WHERE date = DATE('2022-12-31')
+    ),
+    today AS (
+        SELECT user_id,
+               browser_type,
+               CAST(date_trunc('day', event_time) AS DATE) AS event_date,
+               COUNT(1)
+        FROM bootcamp.web_events we
+        JOIN bootcamp.devices d ON d.device_id = we.device_id
+        WHERE date_trunc('day', event_time) = DATE('2023-01-01')
+        GROUP BY user_id, browser_type, CAST(date_trunc('day', event_time) AS DATE)
+    )
+    SELECT COALESCE(y.user_id, t.user_id) AS user_id,
+           COALESCE(y.browser_type, t.browser_type) AS browser_type,
+           CASE
+               WHEN y.dates_active IS NOT NULL THEN ARRAY[t.event_date] || y.dates_active 
+               ELSE ARRAY[t.event_date]
+           END AS dates_active,
+           DATE('2023-01-01') AS date
+    FROM yesterday y
+    FULL OUTER JOIN today t ON y.user_id = t.user_id AND y.browser_type = t.browser_type
     """
     return query
 
@@ -14,7 +37,7 @@ def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_2(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "sumanacheera.user_devices_cumulated"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
@@ -23,3 +46,4 @@ def main():
     )
     output_df = job_2(spark_session, output_table_name)
     output_df.write.mode("overwrite").insertInto(output_table_name)
+
