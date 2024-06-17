8f933dffda1c8db9f072696b6c3b081bb40380dd
Raj <raj@Rajs-MacBook-Air.local>
2024-06-17 17:45:57 -0400

    Homework query update job2.py

--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -4,44 +4,48 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     query = f"""
-WITH yesterday AS (
+
+WITH
+  yesterday AS (
     SELECT
-        host,
-        metric_name,
-        metric_array,
-        month_start
+      host,
+      metric_name,
+      metric_array,
+      CAST(month_start AS DATE) AS month_start
     FROM
-        raj.host_activity_reduced
+      raj.host_activity_reduced
     WHERE
-        month_start = '2023-01-01' 
-),
-today AS (
+      CAST(month_start AS DATE) = DATE '2023-01-01'
+  ),
+  today AS (
     SELECT
-        host,
-        metric_name,
-        CAST(metric_value AS INT) AS metric_value, 
-        date
+      host,
+      metric_name,
+      CAST(FLOOR(CAST(metric_value AS DOUBLE)) AS INT) AS metric_value, -- Adjust casting here
+      CAST(DATE AS DATE) AS DATE
     FROM
-        raj.daily_web_metrics
+      raj.daily_web_metrics
     WHERE
-        date = '2023-01-02' 
-)
+      CAST(DATE AS DATE) = DATE '2023-01-02'
+  )
 SELECT
-    COALESCE(y.host, t.host) AS host, 
-    COALESCE(y.metric_name, t.metric_name) AS metric_name, 
-    COALESCE(
-        y.metric_array, 
-        array_repeat(
-            NULL,
-            CAST(
-                DATEDIFF(CAST('2023-01-01' AS DATE), t.date) AS INT
-            )
-        )
-    ) || ARRAY[t.metric_value] AS metric_array, 
-    '2023-01-01' AS month_start 
+  COALESCE(y.host, t.host) AS host,
+  COALESCE(y.metric_name, t.metric_name) AS metric_name,
+  COALESCE(
+    y.metric_array,
+    TRANSFORM(
+      SEQUENCE(
+        1,
+        ABS(DATE_DIFF('day', DATE '2023-01-01', t.date))
+      ),
+      x -> CAST(NULL AS INTEGER)
+    )
+  ) || ARRAY[t.metric_value] AS metric_array,
+  DATE '2023-01-01' AS month_start
 FROM
-    yesterday y
-    FULL OUTER JOIN today t ON y.host = t.host AND y.metric_name = t.metric_name 
+  yesterday y
+  FULL OUTER JOIN today t ON y.host = t.host
+  AND y.metric_name = t.metric_name
 
     """
 
